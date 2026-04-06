562781558e723bb2a88512b141e6aeafcb743557
ximofam <vienpham177@gmail.com>
2026-04-06 18:26:37 +0700

    refactor: divide responsibility of service and api

--- app/services/payment_service.py
+++ app/services/payment_service.py
@@ -4,7 +4,9 @@ from datetime import datetime
 import stripe.checkout
 from flask import current_app
 
-from app.models import Booking, BookingStatus
+from app import db
+from app.models import Booking, BookingStatus, Ticket
+from app.services.seat_service import SeatService
 
 
 class PaymentService(ABC):
@@ -56,6 +58,53 @@ class StripePaymentService(PaymentService):
             "client_secret": checkout_session.client_secret,
             "public_key": self.public_key
         }
+
+    @staticmethod
+    def verify_event(payload, sig_header, webhook_secret):
+        try:
+            event = stripe.Webhook.construct_event(payload, sig_header, webhook_secret)
+            return event
+        except ValueError:
+            raise ValueError("Invalid payload")
+        except stripe.error.SignatureVerificationError:
+            raise ValueError("Invalid signature")
+
+    @staticmethod
+    def handle_expired_booking(booking, session):
+        stripe.Refund.create(payment_intent=session.payment_intent)
+        if booking.status == BookingStatus.PENDING:
+            booking.status = BookingStatus.CANCELLED
+
+        try:
+            db.session.commit()
+        except Exception:
+            db.session.rollback()
+            raise
+
+    @staticmethod
+    def handle_successful_payment(session):
+        data = session.metadata
+        if not data:
+            raise ValueError("Missing metadata")
+
+        booking = Booking.query.get(data.booking_id)
+        if booking.expires_at <= datetime.now() or booking.status == BookingStatus.CANCELLED:
+            StripePaymentService.handle_expired_booking(booking, session)
+            return
+
+        tickets = [
+            Ticket(booking_id=booking.id, seat_id=seat['id'], price=seat['price'])
+            for seat in booking.seats_data
+        ]
+        db.session.add_all(tickets)
+        booking.status = BookingStatus.PAID
+
+        try:
+            db.session.commit()
+            SeatService.delete_hold_seats_of_booking(booking)
+        except Exception:
+            db.session.rollback()
+            raise
 
 
 class PaymentServiceFactory:
