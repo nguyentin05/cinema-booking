e7552800b91b124938bdd54c08861f584a0780f6
Nguyen Trong Tin <tin.nguyen.cs05@gmail.com>
2026-04-13 22:23:56 +0700

    fix(test): return exact error message

--- tests/unit/test_user_dao.py
+++ tests/unit/test_user_dao.py
@@ -75,61 +75,61 @@ class TestAddUser:
         mock_rollback = mocker.patch.object(db_session, 'rollback')
         mock_logger = mocker.patch("app.daos.user_dao.current_app.logger.error")
 
-        with pytest.raises(Exception, match="Database Connection Dead"):
+        with pytest.raises(Exception, match="^Database Connection Dead$"):
             user_dao.add_user(**valid_user_payload)
 
         mock_rollback.assert_called_once()
-        mock_logger.assert_called_once()
+        mock_logger.assert_called_once_with("Database Connection Dead")
 
     def test_raise_error_with_email_gt_255(self, valid_user_payload, db_session):
         valid_user_payload['email'] = "x" * 255 + "@gmail.com"
 
-        with pytest.raises(ValueError, match="Email must be less than or equal 255 characters"):
+        with pytest.raises(ValueError, match="^Email must be less than or equal 255 characters$"):
             user_dao.add_user(**valid_user_payload)
 
     def test_add_user_email_exists(self, existing_user, valid_user_payload, db_session):
         valid_user_payload['email'] = existing_user.email
 
-        with pytest.raises(ValueError, match="Email already exists"):
+        with pytest.raises(ValueError, match="^Email already exists$"):
             user_dao.add_user(**valid_user_payload)
 
-    @pytest.mark.parametrize("email", [
-        "",
-        "invalid-email",
-        "tests.com",
-        "tests@",
-        "tests@gmail."
+    @pytest.mark.parametrize("email, expected_msg", [
+        ("", "Email is empty"),
+        ("invalid-email", "Invalid email format"),
+        ("tests.com", "Invalid email format"),
+        ("tests@", "Invalid email format"),
+        ("tests@gmail.", "Invalid email format")
     ])
-    def test_add_user_invalid_email(self, valid_user_payload, email):
+    def test_add_user_invalid_email(self, valid_user_payload, email, expected_msg):
         valid_user_payload["email"] = email
 
-        with pytest.raises(ValueError):
+        with pytest.raises(ValueError, match=f"^{expected_msg}$"):
             user_dao.add_user(**valid_user_payload)
 
-    @pytest.mark.parametrize("password", [
-        "",  # Empty
-        'Abc1',  # less than 8 characters
-        '1' + 'a' + 'A' * 127,  # greater than 128 characters
-        'ximofam1',  # no uppercase
-        'XIMOFAM1',  # no lowercase
-        'Ximofamm',  # no digit
-        'ximofamm',  # no uppercase and digit
+    @pytest.mark.parametrize("password, expected_msg", [
+        ("", "Password is empty"),  # Empty
+        ('Abc1', "Password must be at least 8 characters"),  # less than 8 characters
+        ('1' + 'a' + 'A' * 127, "Password must be less than or equal 128 characters"),  # greater than 128 characters
+        ('ximofam1', "Password must contain at least one uppercase letter, one lowercase letter and one digit"),  # no uppercase
+        ('XIMOFAM1', "Password must contain at least one uppercase letter, one lowercase letter and one digit"),  # no lowercase
+        ('Ximofamm', "Password must contain at least one uppercase letter, one lowercase letter and one digit"),  # no digit
+        ('ximofamm', "Password must contain at least one uppercase letter, one lowercase letter and one digit"),  # no uppercase and digit
     ])
-    def test_add_user_invalid_password(self, valid_user_payload, password):
+    def test_add_user_invalid_password(self, valid_user_payload, password, expected_msg):
         valid_user_payload["password"] = password
 
-        with pytest.raises(ValueError):
+        with pytest.raises(ValueError, match=f"^{expected_msg}$"):
             user_dao.add_user(**valid_user_payload)
 
-    @pytest.mark.parametrize("name", [
-        "",
-        "abc",
-        "a" * 51
+    @pytest.mark.parametrize("name, expected_msg", [
+        ("", "Name is empty!!!"),
+        ("abc", "Name must be at least 5 characters"),
+        ("a" * 51, "Name must be less than or equal 50")
     ])
-    def test_add_user_invalid_name(self, valid_user_payload, name):
+    def test_add_user_invalid_name(self, valid_user_payload, name, expected_msg):
         valid_user_payload["name"] = name
 
-        with pytest.raises(ValueError):
+        with pytest.raises(ValueError, match=f"^{expected_msg}$"):
             user_dao.add_user(**valid_user_payload)
 
 
@@ -153,7 +153,7 @@ class TestAuthUser:
     def test_auth_wrong_email(self, existing_user, email):
         password = '123456Abc'
 
-        with pytest.raises(ValueError, match="Incorrect email or password"):
+        with pytest.raises(ValueError, match="^Incorrect email or password$"):
             user_dao.auth_user(email=email, password=password)
 
     @pytest.mark.parametrize("password", [
@@ -164,5 +164,5 @@ class TestAuthUser:
     def test_auth_wrong_password(self, existing_user, password):
         email = existing_user.email
 
-        with pytest.raises(ValueError, match="Incorrect email or password"):
+        with pytest.raises(ValueError, match="^Incorrect email or password$"):
             user_dao.auth_user(email=email, password=password)
